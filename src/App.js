0a8c71f5c107042ceda76cfacc2f3e821d59272a
Ivan Katsenia <ivan.katsenya@gmail.com>
2020-09-02 20:18:41 +0300

    Use class instead of function

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+          baseGreeting: "Good morning!"
+      }
+  }
+
+  render() {
+      return (
+          <div className="App">
+              <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>{ this.state.baseGreeting }</p>
+                  <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                      Learn React
+                  </a>
+                  <button onClick={ () => this.setState({ baseGreeting: "Good night!" })}>
+                      Change greeting text
+                  </button>
+              </header>
+          </div>
+      );
+  }
 }
 
 export default App;
