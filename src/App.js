c8c89ffd7a7fac1660ba7d1bfb2d55b87ef87571
Ivan Katsenia <ivan.katsenya@gmail.com>
2020-09-03 10:35:46 +0300

    Intro to componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
-import * as _ from 'lodash'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-        shoes: [ { name: "loafer", key: "1" }, { name: "brogue", key: "2" }, { name: "boat", key: "3" } ]
-    }
+    this.state = { users: [] }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(users => this.setState({ users: users }))
   }
 
   render() {
     return (
         <div className="App">
             {
-                this.state.shoes.map(shoe => <h1 key={shoe.key}>{_.capitalize(shoe.name)}</h1>)
+                this.state.users.map(user => <h1 key={user.id}>{user.name}</h1>)
             }
         </div>
     );
