1c54e9656d19a6e31815fe8bda7a4ad7f3a1f3bf
Ivan Katsenia <ivan.katsenya@gmail.com>
2020-09-03 10:06:35 +0300

    Set rolodex items with map

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as _ from 'lodash'
 import './App.css';
 
 class App extends Component {
   constructor() {
       super();
       this.state = {
-          baseGreeting: "Good morning!"
+          shoes: [ { name: "loafer", key: "1" }, { name: "brogue", key: "2" }, { name: "boat", key: "3" } ]
       }
   }
 
   render() {
       return (
           <div className="App">
-              <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                  <p>{ this.state.baseGreeting }</p>
-                  <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                      Learn React
-                  </a>
-                  <button onClick={ () => this.setState({ baseGreeting: "Good night!" })}>
-                      Change greeting text
-                  </button>
-              </header>
+              {
+                  this.state.shoes.map(shoe => <h1 key={shoe.key}>{_.capitalize(shoe.name)}</h1>)
+              }
           </div>
       );
   }
