329356a9b63d4c7f1aab1184922477b2a1f8298c
Ivan Katsenia <ivan.katsenya@gmail.com>
2020-09-03 10:11:35 +0300

    Update indents

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 
 class App extends Component {
   constructor() {
-      super();
-      this.state = {
-          shoes: [ { name: "loafer", key: "1" }, { name: "brogue", key: "2" }, { name: "boat", key: "3" } ]
-      }
+    super();
+    this.state = {
+        shoes: [ { name: "loafer", key: "1" }, { name: "brogue", key: "2" }, { name: "boat", key: "3" } ]
+    }
   }
 
   render() {
-      return (
-          <div className="App">
-              {
-                  this.state.shoes.map(shoe => <h1 key={shoe.key}>{_.capitalize(shoe.name)}</h1>)
-              }
-          </div>
-      );
+    return (
+        <div className="App">
+            {
+                this.state.shoes.map(shoe => <h1 key={shoe.key}>{_.capitalize(shoe.name)}</h1>)
+            }
+        </div>
+    );
   }
 }
 
