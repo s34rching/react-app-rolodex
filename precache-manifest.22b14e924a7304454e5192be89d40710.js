1dafeda5531474be163132a609be42155d97e065
Ivan Katsenia <ivan.katsenia@nba-099-11b.local>
2020-09-10 09:30:30 +0300

    Updates

---
+++ precache-manifest.22b14e924a7304454e5192be89d40710.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3e2afddf88a875cca0486b1f1d687e8",
+    "url": "/react-app-rolodex/index.html"
+  },
+  {
+    "revision": "c1db603a6c29e9d1dec0",
+    "url": "/react-app-rolodex/static/css/main.c654c8c3.chunk.css"
+  },
+  {
+    "revision": "2246392ed8b177e3a47e",
+    "url": "/react-app-rolodex/static/js/2.d79e651e.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/react-app-rolodex/static/js/2.d79e651e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1db603a6c29e9d1dec0",
+    "url": "/react-app-rolodex/static/js/main.5c3c795e.chunk.js"
+  },
+  {
+    "revision": "b7fcf1bb4240b4190862",
+    "url": "/react-app-rolodex/static/js/runtime-main.3ef0d571.js"
+  }
+]);
